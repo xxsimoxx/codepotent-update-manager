88506cc55aaf1eb776654295820e985e39bb8ade
xxsimoxx <simospa@gieffeedizioni.it>
2021-06-09 14:49:36 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-#git status
-
-dir=`pwd`
+git status
 
 slug='codepotent-update-manager'
 
 phpfile="${slug}.php"
 
-version=`wp --allow-root eval '$v = get_plugin_data( "'${phpfile}'" ); echo $v["Version"];'`
+version=$(wp --allow-root eval '$v = get_plugin_data( "'${phpfile}'" ); echo $v["Version"];')
 
 echo "Going to release      : v${version}"
-
-
-
-
-
-
-
 
 read -n 1 -s -r -p "If OK, press any key to continue (CTRL-C to exit)."
 
