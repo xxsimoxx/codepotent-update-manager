b107d41f93e5fb6483b0b535e5bae3be695b600d
xxsimoxx <simospa@gieffeedizioni.it>
2022-11-14 16:38:54 +0100

    Bump version

--- codepotent-update-manager.php
+++ codepotent-update-manager.php
@@ -4,7 +4,7 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: Update Manager
  * Description: Painlessly push updates to your ClassicPress plugin users! Serve updates from GitHub, your own site, or somewhere in the cloud. 100% integrated with the ClassicPress update process; slim and performant.
- * Version: 2.4.4
+ * Version: 2.4.5
  * Requires PHP: 5.6
  * Requires CP: 1.0
  * Author: Simone Fioravanti
